315261cf707b37656f62b151b51f9d9d8c1fcfee
Jim Dickinson <jim.dickinson@datastax.com>
2020-03-05 09:30:49 -0500

    KO-288 CassandraDatacenter CRD (#176)
    * Adding a CRD for cassandra.datastax.com/v1alpha2 CassandraDatacener
    
    * More changed bits
    
    Co-authored-by: respringer <respringer@users.noreply.github.com>

---
+++ operator/pkg/apis/cassandra/group.go
@@ -0,0 +1,6 @@
+// Package cassandra contains cassandra API versions.
+//
+// This file ensures Go source parsers acknowledge the cassandra package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package cassandra
---
+++ operator/pkg/apis/cassandra/v1alpha2/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha2 contains API Schema definitions for the cassandra v1alpha2 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=cassandra.datastax.com
+package v1alpha2
