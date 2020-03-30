bd4fb0a6fad0f7d32e14fcc29c517f609951b9b0
Jim Dickinson <jim.dickinson@datastax.com>
2020-03-29 23:08:15 -0400

    Move readyfile above leader election

--- operator/cmd/manager/main.go
+++ operator/cmd/manager/main.go
@@ -100,6 +100,16 @@ func main() {
 
 	ctx := context.Background()
 
+	// Use the operator-sdk ready pkg
+	readyFile := ready.NewFileReady()
+	err = readyFile.Set()
+	if err != nil {
+		log.Error(err, "Problem creating readyFile. Exited non-zero")
+		os.Exit(1)
+	}
+	log.Info("created the readyFile.")
+	defer readyFile.Unset()
+
 	// Become the leader before proceeding
 	err = leader.Become(ctx, "cass-operator-lock")
 	if err != nil {
@@ -164,16 +174,6 @@ func main() {
 	if err != nil {
 		log.Error(err, "could not expose metrics port, continuing anyway")
 	}
-
-	// Use the operator-sdk ready pkg
-	readyFile := ready.NewFileReady()
-	err = readyFile.Set()
-	if err != nil {
-		log.Error(err, "Problem creating readyFile. Exited non-zero")
-		os.Exit(1)
-	}
-	log.Info("created the readyFile.")
-	defer readyFile.Unset()
 
 	log.Info("Starting the Cmd.")
 
