f777f761e88235438d8501833bce1e39db1121ca
Jim Dickinson <jim.dickinson@datastax.com>
2020-04-08 09:10:23 -0400

    GetRacks() moved to the CassandraDatacenter struct

--- operator/pkg/reconciliation/reconcile_racks.go
+++ operator/pkg/reconciliation/reconcile_racks.go
@@ -48,7 +48,7 @@ func (rc *ReconciliationContext) CalculateRackInformation() error {
 	// Create RackInformation
 
 	nodeCount := int(rc.Datacenter.Spec.Size)
-	racks := rc.Datacenter.Spec.GetRacks()
+	racks := rc.Datacenter.GetRacks()
 	rackCount := len(racks)
 
 	// TODO error if nodeCount < rackCount
@@ -834,7 +834,7 @@ func (rc *ReconciliationContext) deleteStuckNodes() (bool, error) {
 func (rc *ReconciliationContext) isClusterHealthy() bool {
 	pods := FilterPodListByCassNodeState(rc.clusterPods, stateStarted)
 
-	numRacks := len(rc.Datacenter.Spec.GetRacks())
+	numRacks := len(rc.Datacenter.GetRacks())
 	for _, pod := range pods {
 		err := rc.NodeMgmtClient.CallProbeClusterEndpoint(pod, "LOCAL_QUORUM", numRacks)
 		if err != nil {
