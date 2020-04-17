b5abc01f45aee20cda7c79d1b67537aef366ea3f
John Trimble <trimblej@gmail.com>
2020-04-17 10:21:47 -0700

    Events for mutations (#43)

--- operator/pkg/apis/cassandra/v1beta1/cassandradatacenter_types.go
+++ operator/pkg/apis/cassandra/v1beta1/cassandradatacenter_types.go
@@ -41,6 +41,23 @@ const (
 	// Progress states for status
 	ProgressUpdating ProgressState = "Updating"
 	ProgressReady    ProgressState = "Ready"
+
+	// Events
+	UpdatingRack                      string = "UpdatingRack"
+	StoppingDatacenter                string = "StoppingDatacenter"
+	DeletingStuckPod                  string = "DeletingStuckPod"
+	RestartingCassandra               string = "RestartingCassandra"
+	CreatedResource                   string = "CreatedResource"
+	StartedCassandra                  string = "StartedCassandra"
+	LabeledPodAsSeed                  string = "LabeledPodAsSeed"
+	UnlabeledPodAsSeed                string = "UnlabeledPodAsSeed"
+	LabeledRackResource               string = "LabeledRackResource"
+	ScalingUpRack                     string = "ScalingUpRack"
+	CreatedSuperuser                  string = "CreatedSuperuser"
+	FinishedReplaceNode               string = "FinishedReplaceNode"
+	ReplacingNode                     string = "ReplacingNode"
+	StartingCassandraAndReplacingNode string = "StartingCassandraAndReplacingNode"
+	StartingCassandra                 string = "StartingCassandra"
 )
 
 // This type exists so there's no chance of pushing random strings to our progress status
