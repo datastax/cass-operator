30e75f4599e9ee2906317e3f1f07d2710fc809ce
Jim Dickinson <jim.dickinson@datastax.com>
2020-04-19 13:09:24 -0400

    Use helm and kubectl to do most of the work to create the yaml bundle

--- hack/release/make-yaml-bundle.sh
+++ hack/release/make-yaml-bundle.sh
@@ -9,31 +9,7 @@ echo '---' >> "$bundle"
 cat operator/deploy/namespace.yaml | yq r - >> "$bundle"
 
 echo '---' >> "$bundle"
-cat operator/deploy/service_account.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/role.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/role_binding.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/cluster_role.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/cluster_role_binding.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/crds/cassandra.datastax.com_cassandradatacenters_crd.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-yq w --doc 3 operator/deploy/operator.yaml 'spec.template.spec.containers[0].image' 'datastax/cass-operator:1.0.0' >> "$bundle"
+helm template ./charts/cass-operator-chart/ -n cass-operator | kubectl create --dry-run=client -o yaml -n cass-operator -f - >> "$bundle"
 
 grep -v x-kubernetes-preserve-unknown-fields < "$bundle" > docs/user/cass-operator-manifests-pre-1.15.yaml
 mv "$bundle" docs/user/cass-operator-manifests.yaml
-
-yq w -d'*' -i docs/user/cass-operator-manifests.yaml metadata.namespace 'cass-operator'
-yq w -d7 -i docs/user/cass-operator-manifests.yaml 'webhooks[*].clientConfig.service.namespace' 'cass-operator'
-#
-yq w -d'*' -i docs/user/cass-operator-manifests-pre-1.15.yaml metadata.namespace 'cass-operator'
-yq w -d7 -i docs/user/cass-operator-manifests-pre-1.15.yaml 'webhooks[*].clientConfig.service.namespace' 'cass-operator'
