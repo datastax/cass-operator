860728cbeb1de529e3eb240a43a70907a71e9722
Jim Dickinson <jim.dickinson@datastax.com>
2020-04-20 12:04:41 -0400

    Enable webhook by default (#46)
    * Set skip to false
    
    * Namespace must be filled in for ClusterRoleBinding
    
    * Removing a bunch of permissions from the ClusterRole
    
    * Add ClusterRole and ClusterRoleBinding to release yaml compiler
    
    * Ran the yaml prep
    
    * Fix usage of default namespace
    
    * Ran yaml prep
    
    * Updating helm chart
    
    * Updating chart CRD
    
    * Use helm and kubectl to do most of the work to create the yaml bundle
    
    * cass-operator is more idiomatic for helm
    
    * Ran through the generation process
    
    * Breaking up this file for easier diffs
    
    * Fixing operator.yaml identing with yq
    
    * Script to help keep helm chart source in line with plain yaml source
    
    * Link to diff-so-fancy
    
    * Put the tool versions in a comment

--- hack/release/make-yaml-bundle.sh
+++ hack/release/make-yaml-bundle.sh
@@ -3,31 +3,15 @@
 set -euf -o pipefail
 set -x
 
+# uses helm v3.1.2, kubectl v1.18.2, yq v3.2.1
+
 bundle="$(mktemp)"
 
 echo '---' >> "$bundle"
 cat operator/deploy/namespace.yaml | yq r - >> "$bundle"
 
 echo '---' >> "$bundle"
-cat operator/deploy/service_account.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/role.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/role_binding.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-cat operator/deploy/crds/cassandra.datastax.com_cassandradatacenters_crd.yaml | yq r - >> "$bundle"
-
-echo '---' >> "$bundle"
-yq w --doc 3 operator/deploy/operator.yaml 'spec.template.spec.containers[0].image' 'datastax/cass-operator:1.0.0' >> "$bundle"
+helm template ./charts/cass-operator-chart/ -n cass-operator | kubectl create --dry-run=client -o yaml -n cass-operator -f - >> "$bundle"
 
 grep -v x-kubernetes-preserve-unknown-fields < "$bundle" > docs/user/cass-operator-manifests-pre-1.15.yaml
 mv "$bundle" docs/user/cass-operator-manifests.yaml
-
-yq w -d'*' -i docs/user/cass-operator-manifests.yaml metadata.namespace 'cass-operator'
-yq w -d7 -i docs/user/cass-operator-manifests.yaml 'webhooks[*].clientConfig.service.namespace' 'cass-operator'
-#
-yq w -d'*' -i docs/user/cass-operator-manifests-pre-1.15.yaml metadata.namespace 'cass-operator'
-yq w -d7 -i docs/user/cass-operator-manifests-pre-1.15.yaml 'webhooks[*].clientConfig.service.namespace' 'cass-operator'
