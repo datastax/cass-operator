506d216f5d268fda390b97929d281ca11889779b
Chris Mills <chris.mills@datastax.com>
2020-05-04 16:48:10 -0400

    remove another unsupported field from pre 1.15 yaml

--- hack/release/make-yaml-bundle.sh
+++ hack/release/make-yaml-bundle.sh
@@ -13,5 +13,5 @@ cat operator/deploy/namespace.yaml | yq r - >> "$bundle"
 echo '---' >> "$bundle"
 helm template ./charts/cass-operator-chart/ -n cass-operator | kubectl create --dry-run=client -o yaml -n cass-operator -f - >> "$bundle"
 
-grep -v x-kubernetes-preserve-unknown-fields < "$bundle" > docs/user/cass-operator-manifests-pre-1.15.yaml
+grep -v "x-kubernetes-preserve-unknown-fields\|matchPolicy" < "$bundle" > docs/user/cass-operator-manifests-pre-1.15.yaml
 mv "$bundle" docs/user/cass-operator-manifests.yaml
