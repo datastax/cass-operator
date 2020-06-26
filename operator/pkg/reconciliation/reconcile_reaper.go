30ac85f3d71886b750414e90476c42394d439026
John Sanda <john.sanda@gmail.com>
2020-06-26 13:24:24 -0400

    Reaper sidecar (#116)
    * initial support for reaper sidecar mode
    
    * debug deployment issues
    
    * using the PodIP for mgmt api calls
    
    * calculate liveness probe init delay based on cluster size
    
    This commit also updates the role binding for helm chart
    
    * create service to expose reaper ui
    
    * fix test failure
    
    * bring back GetPodFromHost function
    
    * add reaper field to CRD
    
    * add checks to see if reaper is enabled and add unit tests for schema job
    
    * add test for buildInitReaperSchemaJob
    
    * add unit tests for reaper service
    
    * bump mgmt api image version
    
    Switch to used mgmt api image v0.1.4 to get the fix for
    https://github.com/datastax/management-api-for-apache-cassandra/issues/18 which
    was blocking me. Without the fix, I could not deploy a CassandraDatacenter and
    then enable Reaper.
    
    Due to the changes in
    https://github.com/datastax/management-api-for-apache-cassandra/commit/1a225f99f91863673b074b3c1a9c1d2d9cd79ecd#diff-3aaf645b2133cebc86740ab63150b507
    I also had to add the IGNORE_DEFAULTS=true env var to the cassandra container
    in order for seeds to get configured properly.
    
    * add integration test and delete service when reaper is disabled
    
    The integration test does the following:
    
    * stand up a cluster
    * enable reaper
    * verify cluster reaches ready state
    * verify that pods include reaper containers
    * disable reaper
    * verify cluster reaches ready state
    * verify pods do not include reaper containers
    
    The CheckReaperService function has been refactored to delete the service if
    reaper is disabled.
    
    * switch default image back to official reaper image
    
    * fix unit test failures
    
    * updates from PR review
    
    * fix bad merge after rebase and add integration test
    
    * removed env var that is no longer needed
    
    * add nil checks since the Reaper field is now a pointer and fix missing back tick

---
+++ operator/pkg/reconciliation/reconcile_reaper.go
@@ -0,0 +1,194 @@
+// Copyright DataStax, Inc.
+// Please see the included license file for details.
+
+package reconciliation
+
+import (
+	"fmt"
+	"github.com/datastax/cass-operator/operator/internal/result"
+	api "github.com/datastax/cass-operator/operator/pkg/apis/cassandra/v1beta1"
+	v1batch "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"math"
+	"strconv"
+)
+
+const (
+	ReaperUIPort             = 7080
+	ReaperAdminPort          = 7081
+	ReaperDefaultImage     = "thelastpickle/cassandra-reaper:2.0.5"
+	ReaperDefaultPullPolicy  = corev1.PullIfNotPresent
+	ReaperContainerName      = "reaper"
+	ReaperHealthCheckPath    = "/healthcheck"
+	ReaperKeyspace           = "reaper_db"
+	ReaperSchemaInitJob      = "ReaperSchemaInitJob"
+	// This code currently lives at https://github.com/jsanda/create_keyspace.
+	ReaperSchemaInitJobImage = "jsanda/reaper-init-keyspace:latest"
+)
+
+func buildReaperContainer(dc *api.CassandraDatacenter) corev1.Container {
+	ports := []corev1.ContainerPort{
+		{Name: "ui", ContainerPort: ReaperUIPort, Protocol: "TCP"},
+		{Name: "admin", ContainerPort: ReaperAdminPort, Protocol: "TCP"},
+	}
+
+	container := corev1.Container{
+		Name: ReaperContainerName,
+		Image: getReaperImage(dc),
+		ImagePullPolicy: getReaperPullPolicy(dc),
+		Ports: ports,
+		LivenessProbe: probe(ReaperAdminPort, ReaperHealthCheckPath, int(60 * dc.Spec.Size), 10),
+		ReadinessProbe: probe(ReaperAdminPort, ReaperHealthCheckPath, 30, 15),
+		Env: []corev1.EnvVar{
+			{Name: "REAPER_STORAGE_TYPE", Value: "cassandra"},
+			{Name: "REAPER_ENABLE_DYNAMIC_SEED_LIST", Value: "false"},
+			{Name: "REAPER_DATACENTER_AVAILABILITY", Value: "SIDECAR"},
+			{Name: "REAPER_SERVER_APP_PORT", Value: strconv.Itoa(ReaperUIPort)},
+			{Name: "REAPER_SERVER_ADMIN_PORT", Value: strconv.Itoa(ReaperAdminPort)},
+			{Name: "REAPER_CASS_CLUSTER_NAME", Value: dc.ClusterName},
+			{Name: "REAPER_CASS_CONTACT_POINTS", Value: fmt.Sprintf("[%s]", dc.GetSeedServiceName())},
+			{Name: "REAPER_AUTH_ENABLED", Value: "false"},
+			{Name: "REAPER_JMX_AUTH_USERNAME", Value: ""},
+			{Name: "REAPER_JMX_AUTH_PASSWORD", Value: ""},
+		},
+	}
+
+	return container
+}
+
+func getReaperImage(dc *api.CassandraDatacenter) string {
+	if len(dc.Spec.Reaper.Image) == 0 {
+		return ReaperDefaultImage
+	}
+	return dc.Spec.Reaper.Image
+}
+
+func getReaperPullPolicy(dc *api.CassandraDatacenter) corev1.PullPolicy {
+	if len(dc.Spec.Reaper.ImagePullPolicy) == 0 {
+		return ReaperDefaultPullPolicy
+	}
+	return dc.Spec.Reaper.ImagePullPolicy
+}
+
+func (rc *ReconciliationContext) CheckReaperSchemaInitialized() result.ReconcileResult {
+	// Using a job eventually get replaced with calls to the mgmt api once it has support for
+	// creating keyspaces and tables.
+
+	rc.ReqLogger.Info("reconcile_reaper::CheckReaperSchemaInitialized")
+
+	if rc.Datacenter.Spec.Reaper == nil || !rc.Datacenter.Spec.Reaper.Enabled {
+		return result.Continue()
+	}
+
+	jobName := getReaperSchemaInitJobName(rc.Datacenter)
+	schemaJob := &v1batch.Job{}
+
+	err := rc.Client.Get(rc.Ctx, types.NamespacedName{Namespace: rc.Datacenter.Namespace, Name: jobName}, schemaJob)
+	if err != nil && errors.IsNotFound(err) {
+		// Create the job
+		schemaJob := buildInitReaperSchemaJob(rc.Datacenter)
+		rc.ReqLogger.Info("creating Reaper schema init job", ReaperSchemaInitJob, schemaJob.Name)
+		if err := setControllerReference(rc.Datacenter, schemaJob, rc.Scheme); err != nil {
+			rc.ReqLogger.Error(err, "failed to set owner reference", ReaperSchemaInitJob, schemaJob.Name)
+			return result.Error(err)
+		}
+		if err := rc.Client.Create(rc.Ctx, schemaJob); err != nil {
+			rc.ReqLogger.Error(err, "failed to create job", ReaperSchemaInitJob, schemaJob.Name)
+			return result.Error(err)
+		} else {
+			return result.RequeueSoon(2)
+		}
+	} else if err != nil {
+		return result.Error(err)
+	} else if jobFinished(schemaJob) {
+		return result.Continue()
+	} else {
+		return result.RequeueSoon(2)
+	}
+}
+
+func getReaperSchemaInitJobName(dc *api.CassandraDatacenter) string {
+	return fmt.Sprintf("%s-reaper-init-schema", dc.Spec.ClusterName)
+}
+
+func getReaperReplication(dc *api.CassandraDatacenter) string {
+	replicationFactor := int(math.Min(float64(dc.Spec.Size), 3))
+	return fmt.Sprintf("{'class': 'NetworkTopologyStrategy', '%s': %d}", dc.Name, replicationFactor)
+}
+
+func jobFinished(job *v1batch.Job) bool {
+	for _, c := range job.Status.Conditions {
+		if (c.Type == v1batch.JobComplete || c.Type == v1batch.JobFailed) && c.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func (rc *ReconciliationContext) CheckReaperService() result.ReconcileResult {
+	rc.ReqLogger.Info("reconcile_reaper::CheckReaperService")
+
+	serviceName := getReaperServiceName(rc.Datacenter)
+	service := &corev1.Service{}
+
+	err := rc.Client.Get(rc.Ctx, types.NamespacedName{Namespace: rc.Datacenter.Namespace, Name: serviceName}, service)
+	if err != nil && errors.IsNotFound(err) {
+		if rc.Datacenter.Spec.Reaper != nil && rc.Datacenter.Spec.Reaper.Enabled {
+			// Create the service
+			service = newReaperService(rc.Datacenter)
+			rc.ReqLogger.Info("creating Reaper service")
+			if err := setControllerReference(rc.Datacenter, service, rc.Scheme); err != nil {
+				rc.ReqLogger.Error(err, "failed to set owner reference", "ReaperService", serviceName)
+				return result.Error(err)
+			}
+			if err := rc.Client.Create(rc.Ctx, service); err != nil {
+				rc.ReqLogger.Error(err, "failed to create Reaper service")
+				return result.Error(err)
+			}
+			return result.Continue()
+		}
+	} else if err != nil {
+		return result.Error(err)
+	} else if rc.Datacenter.Spec.Reaper == nil || !rc.Datacenter.Spec.Reaper.Enabled {
+		if err := rc.Client.Delete(rc.Ctx, service); err != nil {
+			rc.ReqLogger.Error(err, "failed to delete Reaper service", "ReaperService", serviceName)
+		}
+	}
+	return result.Continue()
+}
+
+func getReaperServiceName(dc *api.CassandraDatacenter) string {
+	return fmt.Sprintf("%s-reaper-service", dc.Spec.ClusterName)
+}
+
+func newReaperService(dc *api.CassandraDatacenter) *corev1.Service {
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getReaperServiceName(dc),
+			Namespace: dc.Namespace,
+			Labels:    dc.GetDatacenterLabels(),
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port: ReaperUIPort,
+					Name: "ui",
+					Protocol: corev1.ProtocolTCP,
+					TargetPort: intstr.IntOrString{
+						Type: intstr.String,
+						StrVal: "ui",
+					},
+				},
+			},
+			Selector: dc.GetDatacenterLabels(),
+		},
+	}
+}
---
+++ operator/pkg/reconciliation/reconcile_reaper_test.go
@@ -0,0 +1,152 @@
+package reconciliation
+
+import (
+	api "github.com/datastax/cass-operator/operator/pkg/apis/cassandra/v1beta1"
+	"github.com/stretchr/testify/assert"
+	v1batch "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"testing"
+)
+
+func TestReconcileReaper_buildInitReaperSchemaJob(t *testing.T) {
+	dc := newCassandraDatacenter()
+	job := buildInitReaperSchemaJob(dc)
+
+	assert.Equal(t, getReaperSchemaInitJobName(dc), job.Name)
+	assert.Equal(t, dc.GetDatacenterLabels(), job.Labels)
+
+	assert.Equal(t, 1, len(job.Spec.Template.Spec.Containers))
+	container := job.Spec.Template.Spec.Containers[0]
+
+	assert.Equal(t, ReaperSchemaInitJobImage, container.Image)
+
+	expectedEnvVars := []corev1.EnvVar{
+		{Name: "KEYSPACE", Value: ReaperKeyspace},
+		{Name: "CONTACT_POINTS", Value: dc.GetSeedServiceName()},
+		{Name: "REPLICATION", Value: "{'class': 'NetworkTopologyStrategy', 'ReaperSchemaJobTest': 3}"},
+	}
+	assert.ElementsMatch(t, expectedEnvVars, container.Env)
+}
+
+func TestReconcileReaper_newReaperService(t *testing.T) {
+	dc := newCassandraDatacenter()
+	service := newReaperService(dc)
+
+	assert.Equal(t, getReaperServiceName(dc), service.Name)
+	assert.Equal(t, dc.GetDatacenterLabels(), service.Labels)
+	assert.Equal(t, 1, len(service.Spec.Ports))
+
+	port := service.Spec.Ports[0]
+	assert.Equal(t, int32(ReaperUIPort), port.Port)
+	assert.Equal(t, dc.GetDatacenterLabels(), service.Spec.Selector)
+}
+
+func TestReconcileReaper_CheckReaperSchemaInitialized(t *testing.T) {
+	rc, _, cleanupMockScr := setupTest()
+	rc.Datacenter.Spec.Reaper = &api.ReaperConfig{Enabled: true}
+	defer cleanupMockScr()
+
+	trackObjects := []runtime.Object{rc.Datacenter}
+
+	rc.Client = fake.NewFakeClient(trackObjects...)
+
+	reconcileResult := rc.CheckReaperSchemaInitialized()
+	assert.True(t, reconcileResult.Completed())
+
+	result, err := reconcileResult.Output()
+
+	assert.NoError(t, err)
+	assert.True(t, result.Requeue, "should requeue request")
+
+	job := &v1batch.Job{}
+	jobName := getReaperSchemaInitJobName(rc.Datacenter)
+	err = rc.Client.Get(rc.Ctx, types.NamespacedName{Namespace: rc.Datacenter.Namespace, Name: jobName}, job)
+
+	assert.NoErrorf(t, err, "failed to get job %s", jobName)
+}
+
+func TestReconcileReaper_CheckReaperSchemaNotInitialized(t *testing.T) {
+	rc, _, cleanupMockScr := setupTest()
+	defer cleanupMockScr()
+
+	trackObjects := []runtime.Object{rc.Datacenter}
+
+	rc.Client = fake.NewFakeClient(trackObjects...)
+
+	reconcileResult := rc.CheckReaperSchemaInitialized()
+	assert.False(t, reconcileResult.Completed())
+
+	job := &v1batch.Job{}
+	jobName := getReaperSchemaInitJobName(rc.Datacenter)
+	err := rc.Client.Get(rc.Ctx, types.NamespacedName{Namespace: rc.Datacenter.Namespace, Name: jobName}, job)
+
+	assert.True(t, errors.IsNotFound(err), "did not expect to find job %s", jobName)
+}
+
+func TestReconcileReaper_CheckReaperService(t *testing.T) {
+	rc, _, cleanupMockScr := setupTest()
+	defer cleanupMockScr()
+
+	rc.Datacenter.Spec.Reaper = &api.ReaperConfig{Enabled: true}
+
+	trackObjects := []runtime.Object{rc.Datacenter}
+
+	rc.Client = fake.NewFakeClient(trackObjects...)
+
+	reconcileResult := rc.CheckReaperService()
+	assert.False(t, reconcileResult.Completed())
+
+	service := &corev1.Service{}
+	serviceName := getReaperServiceName(rc.Datacenter)
+	err := rc.Client.Get(rc.Ctx, types.NamespacedName{Namespace: rc.Datacenter.Namespace, Name: serviceName}, service)
+
+	assert.NoErrorf(t, err, "failed to get service %s", serviceName)
+}
+
+func TestReconcileReaper_CheckReaperServiceReaperDisabled(t *testing.T) {
+	rc, _, cleanupMockScr := setupTest()
+	defer cleanupMockScr()
+
+	serviceName := getReaperServiceName(rc.Datacenter)
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: rc.Datacenter.Namespace,
+			Name: serviceName,
+		},
+	}
+
+	trackObjects := []runtime.Object{rc.Datacenter, service}
+
+	rc.Client = fake.NewFakeClient(trackObjects...)
+
+	reconcileResult := rc.CheckReaperService()
+	assert.False(t, reconcileResult.Completed())
+
+	err := rc.Client.Get(rc.Ctx, types.NamespacedName{Namespace: rc.Datacenter.Namespace, Name: serviceName}, service)
+
+	assert.True(t, errors.IsNotFound(err), "did not expect to find service %s", serviceName)
+}
+
+func newCassandraDatacenter() *api.CassandraDatacenter {
+	return &api.CassandraDatacenter{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "ReaperSchemaJobTest",
+			Namespace: "reaper",
+			Labels: map[string]string{
+				api.DatacenterLabel: "ReaperSchemaJobTest",
+				api.ClusterLabel: "reaper",
+			},
+		},
+		Spec: api.CassandraDatacenterSpec{
+			Size:          6,
+			ClusterName:   "reaper",
+			ServerType:    "Cassandra",
+			ServerVersion: "3.11.6",
+		},
+	}
+}
