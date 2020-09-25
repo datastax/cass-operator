f1f237ea6d3127264a014ad3df79d15964a01206
John Trimble <trimblej@gmail.com>
2020-09-25 09:55:07 -0700

    Add arm64 support (#238)

--- mage/sh/lib.go
+++ mage/sh/lib.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"fmt"
 	"strings"
 
 	"github.com/datastax/cass-operator/mage/util"
@@ -21,11 +22,16 @@ import (
 // to print output, if not, output will be hidden.
 // Stderr will work as normal
 func Run(cmd string, args ...string) error {
+	return RunWithEnv(nil, cmd, args...)
+}
+
+func RunWithEnv(env map[string]string, cmd string, args ...string) error {
 	var output io.Writer
 	if mg.Verbose() {
 		output = os.Stdout
 	}
-	_, err := sh.Exec(nil, output, os.Stderr, cmd, args...)
+
+	_, err := sh.Exec(env, output, os.Stderr, cmd, args...)
 	return err
 }
 
@@ -42,14 +48,22 @@ func RunPanic(cmd string, args ...string) {
 
 // Run command and print any output to stdout/stderr
 func RunV(cmd string, args ...string) error {
-	_, err := sh.Exec(nil, os.Stdout, os.Stderr, cmd, args...)
+	return RunVWithEnv(nil, cmd, args...)
+}
+
+func RunVWithEnv(env map[string]string, cmd string, args ...string) error {
+	_, err := sh.Exec(env, os.Stdout, os.Stderr, cmd, args...)
 	return err
 }
 
 // Run command and print any output to stdout/stderr
 // Will automatically panic on error
 func RunVPanic(cmd string, args ...string) {
-	err := RunV(cmd, args...)
+	RunVPanicWithEnv(nil, cmd, args...)
+}
+
+func RunVPanicWithEnv(env map[string]string, cmd string, args ...string) {
+	err := RunVWithEnv(env, cmd, args...)
 	mageutil.PanicOnError(err)
 }
 
@@ -70,8 +84,12 @@ func RunVCapture(cmd string, args ...string) (string, string, error) {
 // Returns output from stdout.
 // stderr gets used as normal here
 func Output(cmd string, args ...string) (string, error) {
+	return OutputWithEnv(nil, cmd, args...)
+}
+
+func OutputWithEnv(env map[string]string, cmd string, args ...string) (string, error) {
 	buf := &bytes.Buffer{}
-	_, err := sh.Exec(nil, buf, os.Stderr, cmd, args...)
+	_, err := sh.Exec(env, buf, os.Stderr, cmd, args...)
 	return strings.TrimSuffix(buf.String(), "\n"), err
 }
 
@@ -83,8 +101,13 @@ func OutputPanic(cmd string, args ...string) string {
 	return out
 }
 
-func cmdWithStdIn(cmd string, in string, args ...string) *exec.Cmd {
+func cmdWithStdIn(env map[string]string, cmd string, in string, args ...string) *exec.Cmd {
+	envArray := []string{}
+	for k, v := range env {
+		envArray = append(envArray, fmt.Sprintf("%s=%s", k, v))
+	}
 	c := exec.Command(cmd, args...)
+	c.Env = envArray
 	buffer := bytes.Buffer{}
 	buffer.Write([]byte(in))
 	c.Stdin = &buffer
@@ -92,7 +115,11 @@ func cmdWithStdIn(cmd string, in string, args ...string) *exec.Cmd {
 }
 
 func RunWithInput(cmd string, in string, args ...string) error {
-	c := cmdWithStdIn(cmd, in, args...)
+	return RunWithEnvWithInput(nil, cmd, in, args...)
+}
+
+func RunWithEnvWithInput(env map[string]string, cmd string, in string, args ...string) error {
+	c := cmdWithStdIn(nil, cmd, in, args...)
 	var output io.Writer
 	if mg.Verbose() {
 		output = os.Stdout
@@ -103,14 +130,27 @@ func RunWithInput(cmd string, in string, args ...string) error {
 }
 
 func RunVWithInput(cmd string, in string, args ...string) error {
-	c := cmdWithStdIn(cmd, in, args...)
+	return RunVWithEnvWithInput(nil, cmd, in, args...)
+}
+
+func RunVWithEnvWithInput(env map[string]string, cmd string, in string, args...string) error {
+	c := cmdWithStdIn(env, cmd, in, args...)
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 	return c.Run()
 }
 
 func OutputWithInput(cmd string, in string, args ...string) (string, error) {
+	return OutputWithEnvWithInput(nil, cmd, in, args...)
+}
+
+func OutputWithEnvWithInput(env map[string]string, cmd string, in string, args ...string) (string, error) {
+	envArray := []string{}
+	for k, v := range env {
+		envArray = append(envArray, fmt.Sprintf("%s=%s", k, v))
+	}
 	c := exec.Command(cmd, args...)
+	c.Env = envArray
 	buffer := bytes.Buffer{}
 	buffer.Write([]byte(in))
 	c.Stdin = &buffer
---
+++ scripts/push-release.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Expected ENV variable inputs:
+#
+# ECR_REPO - the ECR registry (this was misnamed in our GitHub secrets)
+# GITHUB_REF - the git ref of the tag
+# GITHUB_SHA - the git SHA of the current checkout
+#
+
+GIT_TAG="${GITHUB_REF##*/}"
+VERSION=${GIT_TAG#v} # strip the initial 'v' from the tag to get the version
+
+DOCKERHUB_REPOSITORY="datastax/cass-operator"
+ECR_REPOSITORY="${ECR_REPO}/datastax/cass-operator"
+
+# Get the version label of the ECR image so that we can double
+# check that it makes sense for this tag name.
+VERSION_PATH='.Labels["org.label-schema.version"]'
+LABEL_VERSION="$(skopeo inspect "docker://${ECR_REPOSITORY}:${GITHUB_SHA}" | jq "$VERSION_PATH" --raw-output)"
+LABEL_VERSION_UBI="$(skopeo inspect "docker://${ECR_REPOSITORY}:${GITHUB_SHA}-ubi" | jq "$VERSION_PATH" --raw-output)"
+
+# Sanity check. This should never happen.
+if ! [ "$LABEL_VERSION" = "$LABEL_VERSION_UBI" ]; then
+  echo "Standard and UBI images were not labeled with the same version"
+  exit 1
+fi
+
+# Ensure the image has a version appropriate for this tag
+# to prevent confusion.
+#
+# There are two checks in the following if-statement. The
+# first handles the case of a standard release where
+# LABEL_VERSION will contain a "-release" suffix that we
+# generally do not include in the tag. The second handles
+# the case of non-standard releases (a release candidate,
+# alpha, etc.) where we _would_ expect to see the relevant
+# suffix in the tag name.
+if ! [ "v${LABEL_VERSION}" = "${GIT_TAG}-release" ] && ! [ "v${LABEL_VERSION}" = "${GIT_TAG}" ]; then
+  echo "Git tag $GIT_TAG does not align with version number ${LABEL_VERSION}"
+  exit 1
+fi
+
+# Tag images for DockerHub and push them
+for t in "$VERSION" "latest"; do
+  skopeo copy --all "docker://${ECR_REPOSITORY}:${GITHUB_SHA}" "docker://${DOCKERHUB_REPOSITORY}:${t}"
+  skopeo copy --all "docker://${ECR_REPOSITORY}:${GITHUB_SHA}-ubi" "docker://${DOCKERHUB_REPOSITORY}:${t}-ubi"
+done
