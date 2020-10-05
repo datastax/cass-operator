26ee3efa285e1d3b55fbb29a54b0eb79f818a683
Chris Mills <chris.mills@datastax.com>
2020-10-05 15:46:09 -0400

    set up a test for cass 4.0

---
+++ tests/oss_4_0_test_all_the_things/oss_4_0_test_all_the_things_suite_test.go
@@ -0,0 +1,118 @@
+// Copyright DataStax, Inc.
+// Please see the included license file for details.
+
+package oss_4_0_smoke_test
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
+
+	ginkgo_util "github.com/datastax/cass-operator/mage/ginkgo"
+	"github.com/datastax/cass-operator/mage/kubectl"
+)
+
+var (
+	testName     = "OSS 4.0 smoke test"
+	namespace    = "test-oss-4-0-smoke-test"
+	dcName       = "dc1"
+	dcYaml       = "../testdata/oss-three-rack-three-node-4-0-dc.yaml"
+	operatorYaml = "../testdata/operator.yaml"
+	dcResource   = fmt.Sprintf("CassandraDatacenter/%s", dcName)
+	dcLabel      = fmt.Sprintf("cassandra.datastax.com/datacenter=%s", dcName)
+	ns           = ginkgo_util.NewWrapper(testName, namespace)
+)
+
+func TestLifecycle(t *testing.T) {
+	AfterSuite(func() {
+		logPath := fmt.Sprintf("%s/aftersuite", ns.LogDir)
+		kubectl.DumpAllLogs(logPath).ExecV()
+		fmt.Printf("\n\tPost-run logs dumped at: %s\n\n", logPath)
+		ns.Terminate()
+	})
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, testName)
+}
+
+var _ = Describe(testName, func() {
+	Context("when in a new cluster", func() {
+		Specify("the operator can scale up, stop, resume, and terminate a Cass 4.0 dc", func() {
+			By("creating a namespace")
+			err := kubectl.CreateNamespace(namespace).ExecV()
+			Expect(err).ToNot(HaveOccurred())
+
+			step := "setting up cass-operator resources via helm chart"
+			ns.HelmInstall("../../charts/cass-operator-chart")
+
+			ns.WaitForOperatorReady()
+
+			step = "creating a datacenter resource with 3 racks/3 nodes"
+			k := kubectl.ApplyFiles(dcYaml)
+			ns.ExecAndLog(step, k)
+
+			ns.WaitForSuperUserUpserted(dcName, 600)
+
+			step = "check recorded host IDs"
+			nodeStatusesHostIds := ns.GetNodeStatusesHostIds(dcName)
+			Expect(len(nodeStatusesHostIds), 3)
+
+			ns.WaitForDatacenterReady(dcName)
+			ns.WaitForDatacenterCondition(dcName, "Ready", string(corev1.ConditionTrue))
+			ns.WaitForDatacenterCondition(dcName, "Initialized", string(corev1.ConditionTrue))
+
+			step = "scale up to 4 nodes"
+			json := "{\"spec\": {\"size\": 4}}"
+			k = kubectl.PatchMerge(dcResource, json)
+			ns.ExecAndLog(step, k)
+
+			ns.WaitForDatacenterOperatorProgress(dcName, "Updating", 30)
+			ns.WaitForDatacenterReady(dcName)
+
+			step = "scale up to 5 nodes"
+			json = "{\"spec\": {\"size\": 5}}"
+			k = kubectl.PatchMerge(dcResource, json)
+			ns.ExecAndLog(step, k)
+
+			ns.WaitForDatacenterOperatorProgress(dcName, "Updating", 30)
+			ns.WaitForDatacenterReady(dcName)
+
+			step = "stopping the dc"
+			json = "{\"spec\": {\"stopped\": true}}"
+			k = kubectl.PatchMerge(dcResource, json)
+			ns.ExecAndLog(step, k)
+
+			step = "checking the spec size hasn't changed"
+			json = "jsonpath={.spec.size}"
+			k = kubectl.Get(dcResource).
+				FormatOutput(json)
+			ns.WaitForOutputAndLog(step, k, "5", 20)
+
+			ns.WaitForDatacenterToHaveNoPods(dcName)
+
+			step = "resume the dc"
+			json = "{\"spec\": {\"stopped\": false}}"
+			k = kubectl.PatchMerge(dcResource, json)
+			ns.ExecAndLog(step, k)
+
+			ns.WaitForDatacenterReady(dcName)
+
+			ns.ExpectDoneReconciling(dcName)
+
+			step = "deleting the dc"
+			k = kubectl.DeleteFromFiles(dcYaml)
+			ns.ExecAndLog(step, k)
+
+			step = "checking that the dc no longer exists"
+			json = "jsonpath={.items}"
+			k = kubectl.Get("CassandraDatacenter").
+				WithLabel(dcLabel).
+				FormatOutput(json)
+			ns.WaitForOutputAndLog(step, k, "[]", 300)
+		})
+	})
+})
