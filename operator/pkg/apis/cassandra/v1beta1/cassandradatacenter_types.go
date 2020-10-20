b0eae79dae3dc1466ace18b2008d3de81622db5d
Christopher Bradford <christopher.bradford@datastax.com>
2020-10-20 11:27:36 -0400

    Added named ports for other workloads (#289)
    * Added named ports for other workloads
    
    Co-authored-by: Jim Dickinson <jim.dickinson@datastax.com>

--- operator/pkg/apis/cassandra/v1beta1/cassandradatacenter_types.go
+++ operator/pkg/apis/cassandra/v1beta1/cassandradatacenter_types.go
@@ -651,6 +651,56 @@ func (dc *CassandraDatacenter) GetContainerPorts() ([]corev1.ContainerPort, erro
 		namedPort("thrift", 9160),
 	}
 
+	if dc.Spec.ServerType == "dse" {
+		ports = append(
+			ports,
+			namedPort("internode-msg", 8609),
+		)
+	}
+
+	if dc.Spec.DseWorkloads != nil {
+		if dc.Spec.DseWorkloads.AnalyticsEnabled {
+			ports = append(
+				ports,
+				namedPort("spark-app-4040", 4040),
+				namedPort("spark-app-4041", 4041),
+				namedPort("spark-app-4042", 4042),
+				namedPort("spark-app-4043", 4043),
+				namedPort("spark-app-4044", 4044),
+				namedPort("spark-app-4045", 4045),
+				namedPort("spark-app-4046", 4046),
+				namedPort("spark-app-4047", 4047),
+				namedPort("spark-app-4048", 4048),
+				namedPort("spark-app-4049", 4049),
+				namedPort("spark-app-4050", 4050),
+				namedPort("dsefs-public", 5598),
+				namedPort("dsefs-internode", 5599),
+				namedPort("spark-internode", 7077),
+				namedPort("spark-master", 7080),
+				namedPort("spark-worker", 7081),
+				namedPort("jobserver", 8090),
+				namedPort("always-on-sql", 9077),
+				namedPort("jobserver-jmx", 9999),
+				namedPort("sql-thrift", 10000),
+				namedPort("spark-history", 18080),
+			)
+		}
+
+		if dc.Spec.DseWorkloads.GraphEnabled {
+			ports = append(
+				ports,
+				namedPort("gremlin", 8182),
+			)
+		}
+
+		if dc.Spec.DseWorkloads.SearchEnabled {
+			ports = append(
+				ports,
+				namedPort("solr", 8983),
+			)
+		}
+	}
+
 	return ports, nil
 }
 
